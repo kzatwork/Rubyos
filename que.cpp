4717b2db7f4d3ef9b648d6d42a1dbbe0d1de104a
kzatwork <kzatwork@hotmail.com>
2012-11-16 06:32:46 +0900

    add que.cpp

---
+++ que.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+// Task control block
+class TCB {
+
+  int status;
+  int flag;
+  int priority;
+
+  constructor new() {
+
+  }
+
+  delete() {
+
+  }
+
+}
+
+
+// Que control block
+class Que {
+
+  int prev;
+  int next;
+  TCB* tcb;
+
+  int prev {
+    return prev; 
+  }
+
+  int next {
+    return next;
+  } 
+
+}
+
+
+
+
+
