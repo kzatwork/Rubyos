cc6e5cfbba68669251348a0a309121f49103c6f1
kzatwork <kzatwork@hotmail.com>
2012-11-16 18:04:51 +0900

    que.cpp modified.
    Ctor, Dtor update

--- que.cpp
+++ que.cpp
@@ -1,53 +1,64 @@
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 // Task control block
 class TCB {
 
+private:
   int status;
   int flag;
   int priority;
 
-  constructor new() {
-
+public:
+  TCB() {
+    status = 0;
+    flag = 0;
+    priority = 0;
+    cout << "TCB Constructed!" << endl; 
   }
 
-  destructor delete() {
-
+  ~TCB() {
+    cout << "TCB Destructed!" << endl;
   }
 
-}
+};
 
 
 // Que control block
 class Que {
 
-  int prev;
-  int next;
+private:
+  int prev_node;
+  int next_node;
   TCB* tcb;
 
-  constructor new() {
-    prev = 0;
-    next = 0;
-    tcb = TCB.new();
+public:
+
+  Que() {
+    prev_node = 0;
+    next_node = 0;
+    tcb = new TCB;
+    cout << "Que Constructed!" << endl;
   }
 
-  destructor delete() {
-    prev = 0;
-    next = 0;
-    tcb.delete();
+  ~Que() {
+    delete tcb;
+    cout << "Que Destructed!" << endl;
   }
 
-  int prev {
-    return prev; 
+  int prev() {
+    return prev_node; 
   }
 
-  int next {
-    return next;
+  int next() {
+    return next_node;
   } 
 
+};
+
+int main() {
+
+  Que que; 
+  return 0;
 }
-
-
-
-
 
